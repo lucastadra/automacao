d32bc85a56143c25797c0891271ecb9108d440e1
Lucas Tadra Mainginski <lucastadra16@gmail.com>
2021-04-05 01:39:41 -0300

    feat(application): basic navigation config

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
+import "react-native-gesture-handler";
 import React from "react";
 import { View, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Routes from "./src/routes";
 
 const App: React.FC = () => (
-    <>
-        <StatusBar
-            barStyle="light-content"
-            backgroundColor="#152348"
-            translucent
-        />
-        <View style={{ flex: 1, backgroundColor: "#192A56" }} />
-    </>
+    <NavigationContainer>
+        <StatusBar barStyle="light-content" backgroundColor="#152348" />
+        <View style={{ flex: 1, backgroundColor: "#192A56" }}>
+            <Routes />
+        </View>
+    </NavigationContainer>
 );
 
 export default App;
