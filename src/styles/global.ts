ae1f5485089f3d136ee13c9c05326b9de2f22b94
Lucas Tadra Mainginski <lucastadra16@gmail.com>
2021-04-07 23:21:50 -0300

    feat(application): declared color palette and fixed pixel ratio

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,9 +10,21 @@ export const Container = styled.View`
   padding: 0 40px;
 `;
 
+export const colorPalette = {
+  baseWhite: '#FFFFFF',
+  saffronYellow: '#F6B93B',
+  taraweraDarkBlue: '#0A3D62',
+  indigoDarkBlue: '#4A69BD',
+  seashellLightGray: '#F1F1F1',
+  altoMediumGray: '#DADADA',
+  silverDarkGray: 'BDBDBDB',
+  pastelGreen: '#78E08F',
+  keppelBlueyGreen: '#38ADA9',
+};
+
 export const FONT_BASE_SIZE = (): number => {
   if (PixelRatio.get() <= 4) {
-    return 18;
+    return 22;
   }
-  return 24;
+  return 28;
 };
