6c0a32894e10ca5237302cc9d6ab7619c7ce38f6
Lucas Tadra Mainginski <lucastadra16@gmail.com>
2021-04-10 00:50:10 -0300

    test(sentry): added Sentry for code quality & error monitoring

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,2 +1,24 @@
 declare module '*.png';
 declare module '*.svg';
+
+type ReactNavigationTransactionContext = {
+  // ...
+  data: {
+    route: {
+      name: string;
+      key: string;
+      params: {
+        [key: string]: any;
+      };
+      /** Will be true if this is not the first time this screen with this key has been seen before. */
+      hasBeenSeen: boolean;
+    };
+    previousRoute: {
+      name: string;
+      key: string;
+      params: {
+        [key: string]: any;
+      };
+    } | null;
+  };
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import { View, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
+import * as Sentry from '@sentry/react-native';
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#083352" />
-    <View style={{ flex: 1 }}>
-      <Routes />
-    </View>
-  </NavigationContainer>
-);
+const routingInstrumentation = new Sentry.ReactNavigationV5Instrumentation();
+
+Sentry.init({
+  dsn:
+    'https://c543ac7fd5e84a32ac43fa854411cb28@o567794.ingest.sentry.io/5712101',
+  integrations: [
+    new Sentry.ReactNativeTracing({
+      // tracingOrigins: ['localhost', /^\//],
+      routingInstrumentation,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+const App: React.FC = () => {
+  const navigation = useRef<NavigationContainerRef>(null);
+  return (
+    <NavigationContainer
+      ref={navigation}
+      onReady={() => {
+        routingInstrumentation.registerNavigationContainer(navigation);
+      }}
+    >
+      <StatusBar barStyle="light-content" backgroundColor="#083352" />
+      <View style={{ flex: 1 }}>
+        <Routes />
+      </View>
+    </NavigationContainer>
+  );
+};
 
 export default App;
